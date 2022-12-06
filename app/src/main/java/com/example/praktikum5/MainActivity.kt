498a9409e4c57ffd0ec877455f8b41e172c583f0
mimam-h <imamhidayat24@gmail.com>
2022-12-06 13:38:33 +0700

    Menambahkan proyek praktikum5

---
+++ app/src/main/java/com/example/praktikum5/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.praktikum5
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        setCurrentFragment(firstFragment)
+        var navigation_menu = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        navigation_menu.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.miHome -> setCurrentFragment(firstFragment)
+                R.id.miArticle -> setCurrentFragment(secondFragment)
+            }
+            true
+        }
+    }
+
+    override fun <T : View?> findViewById(id: Int): T {
+        return super.findViewById(id)
+    }
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment, fragment)
+            commit()
+        }
+}
